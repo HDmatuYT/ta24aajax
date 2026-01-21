e651fe0b33ff089d645f986b64fe4ff6acfbd25d
HDmatuYT <87945580+HDmatuYT@users.noreply.github.com>
2026-01-21 13:17:49 +0200

    tunnitöö

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
-    let message = 'I need help';
+    import { ref } from 'vue';
+
+    let message = ref('I need help'); //Ref updateib kõike, mis kasutab seda kui see muutub
+
+    function input(event)
+    {
+        console.log(event.target.value);
+        message.value = event.target.value;
+    }
 </script>
 
 <template>
-    <h1>{{ message }}</h1>
+    <div class="container">
+        <h1>{{ message }}</h1>
+        <button class="button is primary" v-on:click="message = 'I need sleep!'">This is a button</button>
+        <input class="input" v-bind:value="message" v-on:input="input">
+    </div>
 </template>
 
 <style>
